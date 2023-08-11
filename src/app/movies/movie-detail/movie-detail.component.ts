a0bf0fd6a870b6d9782e0751a701ad12df73259b
kwonhaewon <kwonhaewon@kwonhaewons-MacBook-Pro.local>
2023-08-11 12:09:10 +0900

    FEAT:27.라우팅 파라미터 건네기

--- src/app/movies/movie-detail/movie-detail.component.ts
+++ src/app/movies/movie-detail/movie-detail.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.scss']
 })
-export class MovieDetailComponent {
+export class MovieDetailComponent implements OnInit {
+  title:string="Movie Detail";
 
+  constructor(private route:ActivatedRoute){
+
+  }
+  
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+    this.title +=`:${id}`
+  }
 }
